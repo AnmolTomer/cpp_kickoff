c00d5471341d47ede2eca0b2ad631a09d80a7354
AnmolTomer <anmol3540@gmail.com>
2019-02-16 22:17:28 +0530

    Add notes for Lect. 1.

---
+++ 01-C++ Introduction _ C ++.cpp
@@ -0,0 +1,28 @@
+// 01-C++ Introduction _ C ++ Tutorial
+
+/* C++ is a general purpose, high-leve, compiler based and object oriented programming language.
+
+General purpose : We are able to design different kinds of software, also called multiple purpose programming language.
+
+E.g.
+(i) Operating Systems : DOS, Windows, Linux etc.
+(ii) Editors : Wordpad, Notepad, MSWord etc.
+(iii) Commercial Applications : College, Hotel, Supermarket, Bank management Systems.
+(iv) Database Designing : Oracle, SQL Server, MYSql etc.
+(iv) Translators : (Convertor) Converts source code to computer understandable object code.
+    Translators are of 3 types viz compiler, interpreter, assembler.
+    Note : Compiler and Interpreter both convert HLL to machine code.
+           Assembler is used to convert LLL to machine code.
+           In C++ we use compiler as translator. Thus, C++ called compiler based language.
+(v) Device Drivers : Printer, Hard drives, GPU drivers A driver provides a software interface to hardware devices,
+    enabling operating systems and other computer programs to access hardware functions without needing to know precise
+    details of the hardware being used.
+
+(vi) PC & Mobile Games : Contra, Mario, Snake etc.
+(vii) Protocols : C++ used extensively for this. Fact : Around 90% of telecom protocols are designed with C++.
+    e.g. HTTP, FTP, Bluetooth transfer protocol.
+
+
+
+
+*/
